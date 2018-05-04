c1436700a4d7ee515e917d955610468a390f3e82
hen3u <loic.hen@telecomnancy.net>
2018-05-04 15:19:09 +0200

    send one string for each event

--- rx_centrale/rx_centrale.ino
+++ rx_centrale/rx_centrale.ino
@@ -6,16 +6,19 @@ RF24 radio(9, 10); // CE, CSN
 
 const byte address[6] = "00001";
 int led_red = 6;
+int led_green = 7;
+int led_orange = 8;
 
 void setup() {
   Serial.begin(9600);
     
   radio.begin();
   radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
   
   pinMode(led_red, OUTPUT);
+  pinMode(led_green, OUTPUT);
+  pinMode(led_orange, OUTPUT);
 }
 
 void loop() {
@@ -24,11 +27,18 @@ void loop() {
     char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);
-    if(strcmp(text,"Hello World")==0){
+    if(strcmp(text,"led_red")==0){
       digitalWrite(led_red, HIGH);
-      delay(1000);
+      delay(500);
+    } else if(strcmp(text,"led_green")==0){
+      digitalWrite(led_green, HIGH);
+      delay(500);
+    } else if(strcmp(text,"led_orange")==0){
+      digitalWrite(led_orange, HIGH);
+      delay(500);
     }
   }
   digitalWrite(led_red, LOW);
-  delay(1000);
+  digitalWrite(led_green, LOW);
+  digitalWrite(led_orange, LOW);
 }
--- tx_centrale/tx_centrale.ino
+++ tx_centrale/tx_centrale.ino
@@ -5,17 +5,25 @@
 RF24 radio(9, 10); // CE, CSN
 
 const byte address[6] = "00001";
+
+const char sensor_red[] = "led_red";
+const char sensor_green[] = "led_green";
+const char sensor_orange[] = "led_orange";
+
+
 void setup() {
   Serial.begin(9600);
 
   radio.begin();
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+  radio.write(&sensor_red, sizeof(sensor_red));
+  delay(1000);
+  radio.write(&sensor_green, sizeof(sensor_green));
+  delay(1000);
+  radio.write(&sensor_orange, sizeof(sensor_orange));
   delay(1000);
 }
